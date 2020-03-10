7e6c8845c1a259b34e672e2a294ce499200ed5ad
Ankit Nigam <ankit082006@gmail.com>
2020-03-11 00:47:14 +0530

    Create udemy_tuple.py

---
+++ Courses/Learning Python Programming/udemy_tuple.py
@@ -0,0 +1,15 @@
+num=[1,2,3,4,5]
+
+x=tuple(num)
+
+print("List :", num)
+print("Tuple :",x)
+
+
+
+my_tuple=('Abc','123',5,"vvv",99)
+x = list(my_tuple)
+
+
+print("List :", x)
+print("Tuple :", my_tuple)
